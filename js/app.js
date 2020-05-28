2317216ec3f2b9a8a501f989be576cda69b6bd1b
ALi Ghodousi <aligodosi@gmail.com>
2020-05-28 07:14:13 +0430

    trip idea done

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const containerFluid = document.querySelector('.container-fluid');
+const main = document.querySelector('.main');
+
+setTimeout(function () {
+    containerFluid.style.display = 'block'
+    main.style.display = 'none';
+}, 10)
