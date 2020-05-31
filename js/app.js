f655e522b015fdc4a3eb8c14cbdd385f5e4dc861
ALi Ghodousi <aligodosi@gmail.com>
2020-05-31 16:25:16 +0430

    Smooth Scrolling

--- js/app.js
+++ js/app.js
@@ -4,4 +4,31 @@ const main = document.querySelector('.main');
 setTimeout(function () {
     containerFluid.style.display = 'block'
     main.style.display = 'none';
-}, 10)
+}, 3000)
+
+
+
+
+
+
+// Smooth Scrolling
+const btn = document.getElementById('btn');
+
+        document.body.onscroll = () => {
+            let top = document.documentElement.scrollTop;
+            if (top > 700) {
+                btn.style.opacity = 1;
+            } else {
+                btn.style.opacity = 0;
+            }
+        }
+
+        btn.onclick = () => {
+            let current = document.documentElement.scrollTop;
+            let scroll = setInterval(() => {
+                window.scrollTo(0, current -= 30)
+                if (current < 1) {
+                    clearInterval(scroll);
+                }
+            }, 5)
+        }
